884fd6ccf0174db1297f4eb5393a11a0d1fff543
Hussein Dawod <56936130+hdtv0199@users.noreply.github.com>
2022-07-11 01:50:11 -0400

    Add files via upload

---
+++ components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () =>{
+    return(
+       <div className="navbar">
+            <div className="nav-elements">
+                <img className="nav-icon" src="images/fill-213.png"/>
+                <p className="nav-title">my travel journal.</p>
+            </div>
+       </div>
+    )
+}
+
+export default Navbar
